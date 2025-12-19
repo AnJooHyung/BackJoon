cb5fc36d4061c8f749cc6f0e15cf9df7ef7f14fe
AnJuHyung <ajh020408@naver.com>
2025-12-20 02:34:16 +0900

    feat BackJoon Level1 clear

---
+++ src/BOJ_1/BackJoon10171/Main.java
@@ -0,0 +1,12 @@
+package BOJ_1.BackJoon10171;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
